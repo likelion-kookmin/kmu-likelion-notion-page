ffdac918e32caa67b710d0d06651a437ce7efcf2
shinkeonkim <singun11@kookmin.ac.kr>
2022-10-15 11:57:30 +0900

    chore: 커스텀 정보 추가

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f934e28e472242e4ae220b94bea3e317',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'kmu-likelion-notion-page',
+  domain: 'https://kmu-likelion-notion-page.vercel.app/',
+  author: 'ShinKeon Kim',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '국민대학교 멋쟁이사자처럼 소개 페이지',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+//  twitter: '',
+  github: 'likelion-kookmin',
+//  linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
